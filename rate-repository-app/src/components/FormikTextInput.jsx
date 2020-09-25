9fb37d90fcc419edf8848263b85469d9e7fdceb9
Viivi Luokkala <viivi.luokkala@edu.turkuamk.fi>
2020-09-25 12:53:20 +0300

    Update erroText alignment

--- rate-repository-app/src/components/FormikTextInput.jsx
+++ rate-repository-app/src/components/FormikTextInput.jsx
@@ -8,7 +8,8 @@ import { Text } from './Text';
 const styles = StyleSheet.create({
   errorText: {
     marginTop: 1,
-    color: 'red'
+    color: 'red',
+    alignSelf: 'center'
   },
 
   formInputBase: {
